2f37c2719509ecba1392a0f040c75779fd0d457c
Amund <Lullinj98@gmail.com>
2023-11-16 17:26:47 +0100

    oppgave 3 A

---
+++ infra/variables.tf
@@ -0,0 +1,19 @@
+variable "prefix" {
+  type = string
+  default = "kandidat2022"
+}
+
+variable "image" {
+  type = string
+  default = "kandidat2022"
+}
+
+variable "imageTag" {
+  type = string
+  default = "latest"
+}
+
+variable "port" {
+  type = string
+  default = "8080"
+}
