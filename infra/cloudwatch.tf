ec09a5d9460fcc79abfd681ae67939697f78c185
Amund <Lullinj98@gmail.com>
2023-11-18 23:52:10 +0100

    awd

--- infra/cloudwatch.tf
+++ infra/cloudwatch.tf
@@ -41,24 +41,32 @@ resource "aws_cloudwatch_dashboard" "main" {
         "title": "Total number of non violations"
       }
     },
-   {
-    "metrics": [
-        [
+    {
+      "type": "metric",
+      "x": 0,
+      "y": 0,
+      "width": 12,
+      "height": 6,
+      "properties": {
+        "metrics": [
+          [
             "${var.prefix}",
-            "danger-violation.value"]
-    ],
-    "period": 300,
-    "stat": "Maximum",
-    "region": "eu-west-1",
-    "title": "DANGER",
-    "view": "gauge",
-    "yAxis": {
-        "left": {
-            "min": 0,
-            "max": 10
+            "danger-violation.value"
+          ]
+        ],
+        "period": 300,
+        "stat": "Maximum",
+        "region": "eu-west-1",
+        "title": "DANGER",
+        "view": "gauge",
+        "yAxis": {
+            "left": {
+                "min": 0,
+                "max": 10
+            }
         }
+      }
     }
-}
   ]
 }
 THEREBEDRAGONS
